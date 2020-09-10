2fa40e8c90bd692206fa7f86575059812d4a8815
stepin aleksey <stepin_a@utkonos.ru>
2020-09-11 02:28:29 +0300

    MVP

---
+++ session.py
@@ -0,0 +1,11 @@
+from user import User
+
+class Session():
+    def __init__(self, id):
+        self.User = User(id)
+        # state = 0 - только стартовали и получили ИД, 1 - показали стили, но не выбрали
+        # 2 - выбран стиль, 3 - есть стили и фотки и можно работать
+        self.state = 0
+        self.content_img_path = None
+        self.style_img_path = None
+
